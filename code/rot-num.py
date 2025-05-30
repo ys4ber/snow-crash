08e163ddf684b0ca1f7dda7bfdcce29105bbbe60
ys4ber <youssefsaber1337@gmail.com>
2025-05-30 18:01:13 +0100

    fixing the code

--- code/rot-num.py
+++ code/rot-num.py
@@ -6,10 +6,14 @@ def main():
         return
     s = sys.argv[1]
     n = int(sys.argv[2])
-    for i in range(len(s)):
-        if s[i] >= 'a' and s[i] <= 'b':
-            s = s[:i] + chr((ord(s[i]) - ord('a') + n) % 26 + ord('a')) + s[i+1:]
-    print(s)
+    res = ""
+    for i in s:
+        if 'a' <= i <= 'z':
+            added = chr(((ord(i) - ord('a') + n) % 26) + ord('a'))
+            res += added
+        else:
+            res += s[i]
+    print(res)
 
 if __name__ == "__main__":
             main()
